ee8b0c19139337013017709ac86b3b984bf360d0
programadoresphicargo <desarrollador@phi-cargo.com>
2024-10-23 17:52:12 -0600

    552

--- lib/conexion/conexion.dart
+++ lib/conexion/conexion.dart
@@ -1,4 +1,4 @@
-String conexion = 'https://phicargo-sistemas.online/phicargo/';
+String conexion = 'https://phides.phicargo-sistemas.online/phicargo/';
 //String conexion = 'http://192.168.10.116/phicargo/';
 //String conexion = 'http://192.168.10.120/phicargo/';
 //String conexion = 'http://192.168.10.138/phicargo/';
