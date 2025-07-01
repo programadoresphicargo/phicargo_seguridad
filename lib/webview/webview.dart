54858267c4229f3800643ee2aca0c693a2d87930
programadoresphicargo <desarrollador@phi-cargo.com>
2025-07-01 11:06:19 -0600

    11.06 am

--- lib/webview/webview.dart
+++ lib/webview/webview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class WebViewExample extends StatefulWidget {
   @override
@@ -8,6 +9,36 @@ class WebViewExample extends StatefulWidget {
 
 class _WebViewExampleState extends State<WebViewExample> {
   late InAppWebViewController webViewController;
+
+  Future<bool> solicitarPermisos(List<Permission> permisos) async {
+    Map<Permission, PermissionStatus> estados = await permisos.request();
+    bool todosConcedidos = estados.values.every((status) => status.isGranted);
+    if (estados.values.any((status) => status.isPermanentlyDenied)) {
+      await openAppSettings();
+    }
+
+    return todosConcedidos;
+  }
+
+  void verificarPermisos() async {
+    bool concedidos = await solicitarPermisos([
+      Permission.camera,
+      Permission.microphone,
+    ]);
+
+    if (concedidos) {
+      print("Permisos concedidos");
+    } else {
+      print("Permisos denegados");
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    verificarPermisos();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +49,20 @@ class _WebViewExampleState extends State<WebViewExample> {
             javaScriptEnabled: true,
             domStorageEnabled: true,
             userAgent: "com.phicargo.admin",
+            mediaPlaybackRequiresUserGesture: false,
+            allowFileAccessFromFileURLs: true,
+            allowUniversalAccessFromFileURLs: true,
+            allowsInlineMediaPlayback: true,
           ),
           initialUrlRequest: URLRequest(
             url: WebUri('https://phides-client.phicargo-sistemas.online/menu'),
           ),
+          onPermissionRequest: (controller, request) async {
+            return PermissionResponse(
+              resources: request.resources,
+              action: PermissionResponseAction.GRANT,
+            );
+          },
           onWebViewCreated: (controller) {
             webViewController = controller;
           },
